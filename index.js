db841c0eac5c603953970b6b194296687e5c43be
vincentLiuxiang <hitliuyaguang@foxmail.com>
2016-09-20 15:52:36 +0800

    default 302

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+'use strict';
+
 var http = require('http');
 
-http.ServerResponse.prototype.redirect = function (url) {
-  this.statusCode = 302;
+http.ServerResponse.prototype.redirect = function (code,url) {
+  if ('string' === typeof code) {
+    url = code;
+    code = 302;
+  }
+  this.statusCode = code;
   this.setHeader('Location',url);
   this.end();
 }
